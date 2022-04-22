fe67179358fab7f25c1f66149099ebd3a64ecc1b
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2022-04-22 12:01:34 +0530

    Time: 212 ms (43.00%), Space: 61 MB (52.13%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,31 +1,98 @@
 class MyHashMap {
 public:
   
-  vector<int>ans;
+  // vector<int>ans;
+  vector<vector<pair<int,int>>>ans;
+  const int size = 10000;
   
   
     MyHashMap() {
       
-      ans.resize(1000001,-1);
+      ans.resize(size);
         
     }
     
     void put(int key, int value) {
-       ans[key]=value;       
+      
+      int ind = key%size;
+      
+      vector<pair<int,int>>&r = ans[ind];
+      
+   for(int i=0;i<r.size();i++)
+        {
+        
+        if(r[i].first==key)
+        {
+          r[i].second=value;
+          return ;
+          
+        }
+        
+      }
+      
+      r.push_back(make_pair(key,value));
+      
+      
+       // ans[key]=value;       
     }
     
     int get(int key) {
         
-    
-        return ans[key];
-     
-     
+       int ind = key%size;
+      
+      vector<pair<int,int>>&r = ans[ind];
+      
+   for(int i=0;i<r.size();i++)
+        {
+        
+           if(r[i].first==key)
+        {
+          
+         
+          return r[i].second;
+             break;
+          
+        }
+        
+      }
+      
+      return -1;
     }
+      
+    
+        // return ans[key];
+     
+     
+    
     
     void remove(int key) {
       
       // ans.erase(ans.begin()  + key);
-      ans[key]=-1;
+       int ind = key%size;
+      
+      vector<pair<int,int>>&r = ans[ind];
+      
+//    for(int i=0;i<r.size();i++)
+//         {
+        
+//         if(r[i].first==key)
+//         {
+//           r.erase(r[i]);
+//           return ;
+          
+//         }
+        
+//       }
+      for (auto iter = r.begin(); iter != r.end(); iter++)
+		{
+			if (iter->first == key)
+			{
+				r.erase(iter);
+                return;
+			}
+		}
+      
+      // ans[key]=-1;
         
     }
 };
