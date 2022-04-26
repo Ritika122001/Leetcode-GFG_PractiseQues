d8257c2f51c139fd1411e66c6dcb9b55a6b29af8
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2022-04-26 13:08:35 +0530

    Time: 1401 ms (16.77%), Space: 108.4 MB (28.54%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,52 +1,135 @@
 class Solution {
 public:
+    
+  
+  
+      void unionf(int u , int v, vector<int>&parent, vector<int>&rank)
+      {
+        
+          u = find(u,parent);
+        v = find(v,parent);
+        
+        if(rank[u] < rank[v])
+        {
+          parent[u] = v;
+        }
+        
+        else if(rank[v] < rank[u])
+        {
+          parent[v] = u;
+        }
+        
+        else{
+          parent[v] = u;
+          rank[u]++;
+        }
+      }
+      
+      int find(int u, vector<int>&parent)
+      {
+        if(u==parent[u])
+        {
+          return u;
+        }
+        
+        return parent[u] = find(parent[u],parent);
+      }
+      
+      
+  
     int minCostConnectPoints(vector<vector<int>>& points) {
       
-      priority_queue< pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>>pq;
-      vector<vector<pair<int,int>>>graph(points.size());
-      vector<bool>visited(points.size(),false);
+      int cost=0;
       
-      for(int i =0;i<points.size();i++)
+      vector<pair<int,pair<int,int>>>graph(points.size());
+      
+  
+            for(int i =0;i<points.size();i++)
       {
         for(int j=i+1;j<points.size();j++)
          { 
           int dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
-          graph[i].push_back({j, dist});
-          graph[j].push_back({i,dist});
+          graph.push_back({dist, {i,j}});
+          graph.push_back({dist , {i,j}});
         }
         
       }
       
+          sort(graph.begin(),graph.end());
       
-      //value and cost
-      pq.push({0,0});
-      int cost=0;
+        vector<int> parent(points.size());
+    vector<int> rank(points.size(),0);
+    
+ 
+      for(int i=0;i<points.size();i++)
+        
+      {
+        parent[i]=i;
+      }
+      
+      
+      for(auto x : graph)
+      {
+        if(find(x.second.first,parent)!=find(x.second.second,parent))
+        {
+          
+          cost+=x.first;
+          unionf(x.second.first , x.second.second, parent , rank);
+        }
+      }
+      
     
       
-      while(!pq.empty())
-      {
+      return cost;
+      
+      
+      
+      
+//       priority_queue< pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>>pq;
+//       vector<vector<pair<int,int>>>graph(points.size());
+//       vector<bool>visited(points.size(),false);
+      
+//       for(int i =0;i<points.size();i++)
+//       {
+//         for(int j=i+1;j<points.size();j++)
+//          { 
+//           int dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+//           graph[i].push_back({j, dist});
+//           graph[j].push_back({i,dist});
+//         }
         
-        pair<int,int>x = pq.top();
-        pq.pop();
-        if(visited[x.second])
-        {
-          continue;
-        }
+//       }
+      
+      
+//     //cost and value  pq will min heap acc to cost
+//       pq.push({0,0});
+//       int cost=0;
+    
+      
+//       while(!pq.empty())
+//       {
+        
+//         pair<int,int>x = pq.top();
+//         pq.pop();
+//         if(visited[x.second])
+//         {
+//           continue;
+//         }
         
        
         
-        visited[x.second]=true;
-         int u = x.second;
-        cost+=x.first;
+//         visited[x.second]=true;
+//          int u = x.second;
+//         cost+=x.first;
         
-        for(auto n : graph[u])
-        {
-          if(!visited[n.first])
-          {
-            pq.push({n.second, n.first});
-          }
+//         for(auto n : graph[u])
+//         {
+//           if(!visited[n.first])
+//           {
+//             pq.push({n.second, n.first});
+//           }
           
-        }
+//         }
         
         
         
@@ -54,9 +137,9 @@ public:
         
         
         
-      }
+//       }
       
-      return cost;
+//       return cost;
       
       
       
