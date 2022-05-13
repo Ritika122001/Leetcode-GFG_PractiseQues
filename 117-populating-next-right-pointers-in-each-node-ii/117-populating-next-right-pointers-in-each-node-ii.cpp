78cf3779dd3af12ef30cd0375d47d4849d1410c5
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2022-05-13 12:05:41 +0530

    Time: 27 ms (16.76%), Space: 17.6 MB (44.54%) - LeetHub

---
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,87 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        
+           if(root==NULL)
+        {
+            return root;
+        }
+        
+        queue<Node*>q;
+        q.push(root);
+        
+        
+        while(!q.empty())
+        {
+            int size = q.size();
+            
+            if(size==0)
+            {
+                return root;
+            }
+            
+         
+            
+          while(size>0)
+          {
+              Node* node;
+              
+              if(size>1)
+              {
+                      
+                
+               node = q.front();
+               q.pop();
+                  
+                  Node* temp = q.front();
+                  node->next = temp;
+        
+            }
+              
+              
+              
+            else{
+                
+                node=q.front();
+                q.pop();
+            }
+              
+                   if(node->left!=NULL)
+            {
+                q.push(node->left);
+            }
+            
+            if(node->right!=NULL)
+            {
+                q.push(node->right);
+            }
+              size--;
+          
+          }
+              
+          }
+        
+        return root;
+              
+        }
+        
+   
+};
