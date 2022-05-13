01555237e85b805acf2230d80a3226f7441d9b09
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2022-05-13 13:17:03 +0530

    Time: 19 ms (49.85%), Space: 17.4 MB (81.04%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -16,72 +16,179 @@ public:
 };
 */
 
+
 class Solution {
+    
 public:
+    
     Node* connect(Node* root) {
         
-           if(root==NULL)
-        {
-            return root;
+        if(root==NULL){
+            return NULL;
         }
         
-        queue<Node*>q;
-        q.push(root);
+        
+        Node* parent=root;
+        Node* childhead;
+        Node* child;
         
         
-        while(!q.empty())
+        while(parent!=NULL)
         {
-            int size = q.size();
-            
-            if(size==0)
+            while(parent!=NULL)
             {
-                return root;
+                
+                      
+            if(parent->left)
+            {
+                if(childhead==NULL)
+                {
+                    childhead=parent->left;
+                    
+                }
+                
+                else{
+                    child->next = parent->left;
+                    
+                    
+                }
+                
+                child = parent->left;
             }
+            
+            
+            
+            
+              if(parent->right)
+            {
+                if(childhead==NULL)
+                {
+                    childhead=parent->right;
+                    
+                }
+                
+                else{
+                    
+                    child->next = parent->right;
+                    
+                    
+                }
+                
+                child = parent->right;
+            }
+            
+            
+            parent = parent->next;
+            }
+            
+            
+      
+            parent = childhead;
+            childhead=child=NULL;
+            
+            
+            
+            
+        }
+    
+   
+        
+        
+//            if(root==NULL)
+//         {
+//             return root;
+//         }
+        
+//         queue<Node*>q;
+//         q.push(root);
+        
+        
+//         while(!q.empty())
+//         {
+//             int size = q.size();
+            
+//             if(size==0)
+//             {
+//                 return root;
+//             }
             
          
             
-          while(size>0)
-          {
-              Node* node;
+//           while(size>0)
+//           {
+//               Node* node;
               
-              if(size>1)
-              {
+//               if(size>1)
+//               {
                       
                 
-               node = q.front();
-               q.pop();
+//                node = q.front();
+//                q.pop();
                   
-                  Node* temp = q.front();
-                  node->next = temp;
+//                   Node* temp = q.front();
+//                   node->next = temp;
         
-            }
+//             }
               
               
               
-            else{
+//             else{
                 
-                node=q.front();
-                q.pop();
-            }
+//                 node=q.front();
+//                 q.pop();
+//             }
               
-                   if(node->left!=NULL)
-            {
-                q.push(node->left);
-            }
+//                    if(node->left!=NULL)
+//             {
+//                 q.push(node->left);
+//             }
             
-            if(node->right!=NULL)
-            {
-                q.push(node->right);
-            }
-              size--;
+//             if(node->right!=NULL)
+//             {
+//                 q.push(node->right);
+//             }
+//               size--;
           
-          }
+//           }
               
-          }
+//           }
+        
+//         return root;
+              
+//         }
+        
+        
+//         if(root==NULL)
+//         {
+//             return NULL;
+//         }
+        
+        
+//         Node* node = root;
+        
+//         while(node->left!=NULL)
+//         {
+            
+//             Node* temp  = node;
+            
+//             while(node!=NULL)
+//             {
+                
+//                 node->left->next  = node->right;
+//                 node->right->next = node->next==NULL ? NULL : node->next->left;
+//                 node=  node->next;
+                
+//             }
+            
+//             node= temp->left;
+            
+//         }
+        
+        
         
         return root;
-              
-        }
         
+    }  
+      
    
 };
