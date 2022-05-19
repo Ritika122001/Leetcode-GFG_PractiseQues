cea22d5d9b187c93fb564c15afb80f185364570d
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2022-05-19 13:33:13 +0530

    Time: 90 ms (50.66%), Space: 26.3 MB (38.20%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -33,24 +33,57 @@ public:
         
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> memo( n, vector<int>(m, -1));
-        
+        vector<vector<int>> memo(n,vector<int>(m,0));
+    
+        for(int i=0;i<m;i++)
+        {
+            memo[0][i] = matrix[0][i];
+            
+        }
         
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
+            memo[i][0] = matrix[i][0];
+        }
+        
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
             {
-                if(matrix[i][j]==1)
+                
+                if(matrix[i][j]==0)
                 {
-                     res+=  solve(matrix,i,j,n,m,memo);
-                    // res = max( solve(matrix,i,j,n,m,memo), res );
+                    memo[i][j]=0;
                 }
+             
+                else{
+                     memo[i][j] =1 +  min(memo[i-1][j] , min( memo[i][j-1] , memo[i-1][j-1]));
+                    
+                }
+                
+               
+                   
+                
+                
+                // if(matrix[i][j]==1)
+                // {
+                //      res+=  solve(matrix,i,j,n,m,memo);
+                //     // res = max( solve(matrix,i,j,n,m,memo), res );
+                // }
             }
         
         
         }
         
-        return res;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            res+= memo[i][j];
+        }
+    }
         
+        
+        return res;
     }
 };
