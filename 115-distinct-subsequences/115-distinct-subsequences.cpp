1fa7828b5eef89ac2e2e95c54bec4656ec2df34f
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2022-05-28 19:58:02 +0530

    Time: 50 ms (52.34%), Space: 18.5 MB (46.68%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -3,37 +3,37 @@ public:
     
     
     
-    int solve(string &s, string &t, int i , int j , vector<vector<int>>&memo)
-    {
-        if(j<=0)
-        {
-            return 1;
-        }
+//     int solve(string &s, string &t, int i , int j , vector<vector<int>>&dp)
+//     {
+//         if(j<=0)
+//         {
+//             return 1;
+//         }
         
-        if(i<=0)
-        {
-            return 0;
-        }
+//         if(i<=0)
+//         {
+//             return 0;
+//         }
         
-        if(memo[i][j]!=-1)
-        {
-            return memo[i][j];
-        }
+//         if(memo[i][j]!=-1)
+//         {
+//             return memo[i][j];
+//         }
         
         
-        if( s[i-1] == t[j-1] )
-        {
+//         if( s[i-1] == t[j-1] )
+//         {
             
-            return memo[i][j] = solve(s,t,i-1,j-1,memo) + solve(s,t,i-1,j,memo);
+//             return memo[i][j] = solve(s,t,i-1,j-1,memo) + solve(s,t,i-1,j,memo);
             
-        }
+//         }
         
-        else
-            return memo[i][j] =  solve(s,t,i-1,j,memo);
+//         else
+//             return memo[i][j] =  solve(s,t,i-1,j,memo);
         
         
     
-    }
+//     }
     
     
     
@@ -42,8 +42,58 @@ public:
         int n = s.length();
         int m = t.length();
         
-        vector<vector<int>>memo(n+1, vector<int>(m+1,-1));
-        return solve(s,t,s.length(),t.length(),memo);
+        // vector<vector<int>>memo(n+1, vector<int>(m+1,-1));
+        vector<vector<unsigned int>>dp(n+1, vector<unsigned int>(m+1,0));
+        
+        
+        
+        
+        for(int i=0;i<=n;i++)
+
+        {
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0 and j==0)
+                {
+                    dp[i][j]=1;
+                }
+                
+                else if(i==0)
+                {
+                    dp[i][j]=0;
+                }
+                
+                else if(j==0)
+                {
+                    dp[i][j]=1;
+                }
+                
+                else{
+                       
+        if( s[i-1] == t[j-1] )
+        {
+            
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+            
+        }
+        
+        else
+        {
+             dp[i][j] =  dp[i-1][j];
+            
+        }
+           
+                }
+                    
+            }
+        }
+        
+        
+        
+        
+        
+        return dp[n][m];
+        // return solve(s,t,s.length(),t.length(),dp);
         
     }
 };
