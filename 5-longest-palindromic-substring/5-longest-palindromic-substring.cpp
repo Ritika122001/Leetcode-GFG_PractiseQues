83ca81b69edeb1a1fdbcedf57fd88e251e1d20e0
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2022-06-16 11:59:32 +0530

    Time: 45 ms (69.42%), Space: 7.2 MB (81.58%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,106 +2,149 @@ class Solution {
     
 public:
     
-
+    int expand(string &s, int i , int j)
+    {
+        
+        
+        while(i>=0 and j<s.length() and s[i]==s[j])
+        {
+            i--;
+            j++;
+        }
+        
+        return j-i-1;
+        
+        
+        
+    }
+    
+     string longestPalindrome(string s) {
+         
+         
+         int start;
+         int maxilen =0;
+         
+         for(int i=0;i<s.length();i++)
+         {
+             int evenlen = expand(s,i,i+1);
+             int oddlen = expand(s,i,i);
+             int len = max(evenlen , oddlen);
+             if(maxilen < len)
+             {
+                 maxilen = len;
+                 start = i - (len-1)/2;
+             }
+             
+             
+             
+             
+         }
+         
+         
+         return s.substr(start,maxilen);
+         
+         
+     }
+   
     
     
     //do lcs of both string : - string itself and reverse of it
     
-    string longestPalindrome(string s) {
-                int fStart; 
-        int n = s.length();
-        string r = s;
-        reverse(r.begin(), r.end());
-        vector<vector<int>>dp(n+1,vector<int>(n+1));
+//     string longestPalindrome(string s) {
+//                 int fStart; 
+//         int n = s.length();
+//         string r = s;
+//         reverse(r.begin(), r.end());
+//         vector<vector<int>>dp(n+1,vector<int>(n+1));
          
-        int mini=INT_MIN;
-        string ans;
+//         int mini=INT_MIN;
+//         string ans;
         
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=n;j++)
-            {
-                if(i==0 || j==0)
-                {
-                    dp[i][j]=0;
-                }
-                
-                else if(s[i-1]==r[j-1])
-                {
-                    
-                    dp[i][j] = 1  + dp[i-1][j-1];
-                    
-//                     if(dp[i][j]>mini)
-//                     {
-//                         string temp = s.substr(i-dp[i][j], dp[i][j]);
-//                          string res = string(temp.rbegin(),temp.rend());
-//                         if(res==temp)
-//                         {
-//                             mini = dp[i][j] ;
-//                             ans = temp ;
-//                         }
-//                     }
-                    
-                }
-                
-                else
-                {
-                    dp[i][j] =0;
-                }
-                
-                if(dp[i][j] > mini)
-                {
-                    
-                    int start = (i-1)-dp[i][j] + 1;
-                     int rstart = (j-1)-dp[i][j] + 1;
-                    int cstart = (n-1) - rstart;
-                    if(cstart == (i-1) && (n-1)-(j-1) ==start)
-                    {
-                        mini = dp[i][j];
-                        fStart = (i-1)-dp[i][j] + 1;
-                    }          
-                }
-                
-                  
-            }
-        }
-        
-            return s.substr(fStart,mini);
-    }
-    
-
-    
-//         lcs(s,r ,dp,n);
-        
-        
-//         int i = n;
-//         int j = n;
-// string ans;
-        
-//         while(i>0 and j>0)
+//         for(int i=0;i<=n;i++)
 //         {
-//             if(s[i-1]==r[j-1])
+//             for(int j=0;j<=n;j++)
 //             {
-//                 ans.push_back(s[i-1]);
-//                 i--;
-//                 j--;
-                
-//             }
-            
-//             else {
-                 
-//                 if(s[i][j-1]  > s[i-1][j])
+//                 if(i==0 || j==0)
 //                 {
-//                     j--;
+//                     dp[i][j]=0;
+//                 }
+                
+//                 else if(s[i-1]==r[j-1])
+//                 {
+                    
+//                     dp[i][j] = 1  + dp[i-1][j-1];
+                    
+// //                     if(dp[i][j]>mini)
+// //                     {
+// //                         string temp = s.substr(i-dp[i][j], dp[i][j]);
+// //                          string res = string(temp.rbegin(),temp.rend());
+// //                         if(res==temp)
+// //                         {
+// //                             mini = dp[i][j] ;
+// //                             ans = temp ;
+// //                         }
+// //                     }
+                    
 //                 }
                 
 //                 else
-//                     i--;
+//                 {
+//                     dp[i][j] =0;
+//                 }
                 
+//                 if(dp[i][j] > mini)
+//                 {
+                    
+//                     int start = (i-1)-dp[i][j] + 1;
+//                      int rstart = (j-1)-dp[i][j] + 1;
+//                     int cstart = (n-1) - rstart;
+//                     if(cstart == (i-1) && (n-1)-(j-1) ==start)
+//                     {
+//                         mini = dp[i][j];
+//                         fStart = (i-1)-dp[i][j] + 1;
+//                     }          
+//                 }
+                
+                  
 //             }
 //         }
         
-//         return ans;
+//             return s.substr(fStart,mini);
+//     }
+    
+
+    
+// //         lcs(s,r ,dp,n);
+        
+        
+// //         int i = n;
+// //         int j = n;
+// // string ans;
+        
+// //         while(i>0 and j>0)
+// //         {
+// //             if(s[i-1]==r[j-1])
+// //             {
+// //                 ans.push_back(s[i-1]);
+// //                 i--;
+// //                 j--;
+                
+// //             }
+            
+// //             else {
+                 
+// //                 if(s[i][j-1]  > s[i-1][j])
+// //                 {
+// //                     j--;
+// //                 }
+                
+// //                 else
+// //                     i--;
+                
+// //             }
+// //         }
+        
+// //         return ans;
         
         
 };
