792d3973af8e385cc04d930fe4c3bd3aad88795f
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2022-06-16 12:28:07 +0530

    Added solution - LeetHub

--- Smallest distinct window - GFG/smallest-distinct-window.cpp
+++ Smallest distinct window - GFG/smallest-distinct-window.cpp
@@ -8,61 +8,121 @@ class Solution{
     public:
     int findSubString(string str)
     {
-        // Your code goes here 
-        unordered_set<char>st;
+        
+        unordered_map<char,int>mp1;
+        unordered_map<char,int>mp2;
+        int ans=INT_MAX;
+        
+        
         for(int i=0;i<str.length();i++)
         {
-            st.insert(str[i]);
+            mp1[str[i]]++;
         }
-    
-        unordered_map<char,int>mp;
-        int i=0;
-        int j=0;
-        int cnt=0;
-        int n1 = str.length();
-        int n2 = st.size();
-        int mini = INT_MAX;
-        while(j<n1)
-        {
-            if(cnt<n2)
-            {
-                if(mp[str[j]]==0)  cnt++;
-          
-                mp[str[j]]++;
-                j++;
         
+        int i=0;
+        
+        for(int j=0 ;j<str.length();j++)
+        {
+            
+            mp2[str[j]]++;
+            if(mp2.size()==mp1.size())
+            {
+                
+                
+                while(mp2[str[i]]> 1)
+                {
+                    mp2[str[i]]--;
+                    i++;
+                }
+                
+                ans = min(ans,j-i+1);
             }
             
          
-                
-                while(cnt==n2)
-                {
-                    
-                    
-                mini = min(mini,j-i);
-                if(mp[str[i]]==1)
-                {
-                  cnt--;
-                    //   mp.erase(str[i]);
-                    
-                }
-                
-                mp[str[i]]--; //if freq is greater than 1 
-                i++;
-               
-                
-                }
-      
-   
-        }
-        
-        if(mini==INT_MAX)
-        {
-            return -1;
             
         }
         
- return mini;
+        return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // Your code goes here 
+//         unordered_set<char>st;
+//         for(int i=0;i<str.length();i++)
+//         {
+//             st.insert(str[i]);
+//         }
+    
+//         unordered_map<char,int>mp;
+//         int i=0;
+//         int j=0;
+//         int cnt=0;
+//         int n1 = str.length();
+//         int n2 = st.size();
+//         int mini = INT_MAX;
+//         while(j<n1)
+//         {
+//             if(cnt<n2)
+//             {
+//                 if(mp[str[j]]==0)  cnt++;
+          
+//                 mp[str[j]]++;
+//                 j++;
+        
+//             }
+            
+         
+                
+//                 while(cnt==n2)
+//                 {
+                    
+                    
+//                 mini = min(mini,j-i);
+//                 if(mp[str[i]]==1)
+//                 {
+//                   cnt--;
+//                     //   mp.erase(str[i]);
+                    
+//                 }
+                
+//                 mp[str[i]]--; //if freq is greater than 1 
+//                 i++;
+               
+                
+//                 }
+      
+   
+//         }
+        
+//         if(mini==INT_MAX)
+//         {
+//             return -1;
+            
+//         }
+        
+//  return mini;
 
         
         
