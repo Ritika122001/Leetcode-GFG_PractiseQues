30cc51dd85cdc25ad712913bbfc001951b89cffb
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2022-08-03 11:42:26 +0530

    Time: 109 ms (95.70%), Space: 33.7 MB (98.08%) - LeetHub

--- 731-my-calendar-ii/731-my-calendar-ii.cpp
+++ 731-my-calendar-ii/731-my-calendar-ii.cpp
@@ -1,38 +1,72 @@
 class MyCalendarTwo {
 public:
     
-   map<int,int>mp;
+   // map<int,int>mp;
+    
+     vector<pair<int,int>>booking;
+      vector<pair<int,int>>doublebooking;
         
     
     MyCalendarTwo() {
         
     }
     
+    
+    bool check(int s1 , int s2 , int e1 , int e2)
+    {
+        return max(s1,s2) < min(e1,e2);
+    }
+    
     bool book(int start, int end) {
+    
+       
         
-      
-        mp[start]++;
-        mp[end]--;
-        
-         int sum = 0;
-        
-        for(auto c:mp)
+        for(auto c : doublebooking)
         {
-            sum+=c.second;
-              if(sum>2)
-        {
-            mp[start]--;
-            mp[end]++;
-            return false;
+            
+            // if(check(c.first,c.second,start,end))
+            if(start<c.second and end>c.first)
+                  return false;
+
+        
         }
+        
+        
+        for(auto c : booking)
+        {
+            // if(check(c.first,c.second,start,end))
+            if(start<c.second && end >c.first)
+            {
+                doublebooking.push_back( { max(c.first,start) , min(end,c.second)});
+            }
             
         }
         
-      
-        
+        booking.push_back({start,end});
         return true;
         
     }
+        
+      
+//         mp[start]++;
+//         mp[end]--;
+        
+//          int sum = 0;
+        
+//         for(auto c:mp)
+//         {
+//             sum+=c.second;
+//               if(sum>2)
+//         {
+//             mp[start]--;
+//             mp[end]++;
+//             return false;
+//         }
+            
+//         }
+//         return true;
+        
+//     }
 };
 
 /**
