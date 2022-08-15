4f8b9991ecb82c4be1582b9ab82ae1a3cf1cfd5a
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2022-08-15 13:36:48 +0530

    Time: 41 ms (10.18%), Space: 8.1 MB (37.92%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+    int romanToInt(string s) {
+        
+    unordered_map<char,int>mp;
+    mp['I'] = 1;
+        mp['V']=5;
+        mp['X']=10;
+        mp['L']= 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M']= 1000;
+        
+        int ans = 0;
+        
+        
+        
+       for(int i=0;i<s.length();)
+       {
+           
+           if( mp[s[i]] < mp[s[i+1]] and i+1<s.length() )
+           {
+               
+               ans+=mp[s[i+1]]-mp[s[i]];
+               i+=2;
+           }
+           
+           else{
+               ans+=mp[s[i]];
+               i++;
+           }
+           
+           
+       }
+        
+        
+        return ans;
+        
+        
+    }
+};
