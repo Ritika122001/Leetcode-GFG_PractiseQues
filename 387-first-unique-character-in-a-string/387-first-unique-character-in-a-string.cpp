7ab89ebb8191868752151e26334d99e027dc30a3
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2022-08-16 11:38:34 +0530

    Time: 95 ms (16.70%), Space: 10.7 MB (71.24%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -2,27 +2,64 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         
-        vector<int>res(26,0);
+        // 1st Approach
+//         vector<int>res(26,0);
         
         
-        for(int i=0;i<s.length();i++)
-        {
+//         for(int i=0;i<s.length();i++)
+//         {
             
-            res[s[i]-'a']++;
+//             res[s[i]-'a']++;
             
             
-        }
+//         }
         
-        for(int i=0;i<s.length();i++)
-        {
-            if(res[s[i]-'a']==1)
-            {
-                return i;
+//         for(int i=0;i<s.length();i++)
+//         {
+//             if(res[s[i]-'a']==1)
+//             {
+//                 return i;
                 
-            }
+//             }
+//         }
+        
+//         return -1;
+        
+        
+        
+//         2nd Approach
+        
+        unordered_map<char,pair<int,int>>mp;
+        
+        for(int i=0;i<s.length();i++)
+        {
+            
+         mp[s[i]].first++;
+        mp[s[i]].second= i;
+            
+            
         }
         
-        return -1;
+        
+        int ind=s.length();
+        
+        for(auto [c,m] : mp)
+        {
+            
+            if(m.first==1)
+            {
+                ind = min(ind,m.second);
+            }
+            
+            
+        }
+        
+        return ind ==s.length() ? -1 : ind;
+        
+        
+        
+        
+        
         
         
     }
