892da6abefa72f8374014207c3183011b5a931d0
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2022-09-14 13:48:08 +0530

    Time: 22 ms (47.01%), Space: 14.5 MB (10.72%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -23,21 +23,21 @@ public:
         
         for(int i=prices.size()-1;i>=0;i--)
         {
-            for(int buy=0;buy<=1;buy++)
-            {
+            // for(int buy=0;buy<=1;buy++)
+            // {
                 for(int cap=1;cap<=k;cap++)
                 {
                         
-                    if(buy==1)
-                    {
-                        dp[i][buy][cap] = max( (-prices[i] + dp[i+1][0][cap]) , ( 0 + dp[i+1][1][cap]) );
-                    }
-                    else
-                    {
-                        dp[i][buy][cap] = max(prices[i] + dp[i+1][1][cap-1] , ( 0 + dp[i+1][0][cap]) );
-                    }
+                    // if(buy==1)
+                    // {
+                        dp[i][1][cap] = max( (-prices[i] + dp[i+1][0][cap]) , ( 0 + dp[i+1][1][cap]) );
+                    // }
+                    // else
+                    // {
+                        dp[i][0][cap] = max(prices[i] + dp[i+1][1][cap-1] , ( 0 + dp[i+1][0][cap]) );
+                    // }
                     
-                }
+                // }
             }
         }
         
