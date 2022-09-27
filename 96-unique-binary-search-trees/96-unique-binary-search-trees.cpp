c73505c3f5f38fde3b5eae85dae8659da68d1694
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2022-09-27 19:56:13 +0530

    Time: 2559 ms (8.36%), Space: 6 MB (69.70%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+ 
+    
+    
+    int numTrees(int n) {
+        
+         if(n<=1)
+        {
+            return 1;
+        }   
+        
+        
+        if(n==2)
+        {
+            return 2;
+        }
+        
+        int ans=0;
+    
+       for(int i=1;i<=n;i++){
+         ans+=numTrees(i-1)*numTrees(n-i);
+  
+}
+        
+        return ans;
+    }
+};
