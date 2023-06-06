0897f8936d0e6bebee6785ff87cbcd6dd4dda6cc
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2023-06-06 20:26:30 +0530

    Time: 8 ms (25.46%), Space: 9.9 MB (5.60%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -2,18 +2,50 @@ class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) 
     {
-        sort(arr.begin() ,arr.end());
-        
-        int d = arr[1] - arr[0];
-        for(int i=2; i<arr.size();i++)
+        vector<int>r;
+        unordered_set<int>st;
+        for(auto c : arr)
         {
-            if(arr[i] - arr[i-1]!=d)
+            st.insert(c);
+            r.push_back(c);
+        }
+        
+        sort(r.begin() ,r.end());
+
+
+        
+        int a = r[0];
+        int an = r[arr.size()-1];
+        int n = arr.size()-1;
+        
+
+        if( ((an-a) % n)!=0) return false;
+        int d = (an-a)/(n);
+        
+        int i=0;
+        while(i<r.size())
+        {
+            int num = a + i*d;
+            if(st.find(num) == st.end())
             {
                 return false;
             }
+            i++;
+            
         }
         
+    return true;
         
-        return true;
+//         int d = arr[1] - arr[0];
+//         for(int i=2; i<arr.size();i++)
+//         {
+//             if(arr[i] - arr[i-1]!=d)
+//             {
+//                 return false;
+//             }
+//         }
+        
+        
+//         return true;
     }
 };
