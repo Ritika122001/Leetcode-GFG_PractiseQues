1337e29a34ee61f04b97e2be08bd5922d3cab643
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2023-07-05 20:39:36 +0530

    Time: 5 ms (85.78%), Space: 10.1 MB (20.84%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        unordered_map<int,int>mp;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        
+        for(auto c : mp)
+        {
+            if(c.second==1)
+            { 
+               return c.first;
+            }
+            
+        }
+        
+        return 1;
+    }
+};
