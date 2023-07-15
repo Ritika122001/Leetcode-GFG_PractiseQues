67f8d6364b4627a3599f1bf3003e7f18ac25dc6d
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2023-07-15 13:20:30 +0530

    Time: 300 ms (90.99%), Space: 75.9 MB (58.61%) - LeetHub

---
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+
+public:
+    
+    int recur(vector<vector<int>>&dp,int idx,vector<vector<int>>&events,int k)
+    {
+        if(idx>=events.size() or k==0)
+            return 0;
+        int i;
+        
+        if(dp[idx][k]!=-1)
+            return dp[idx][k];
+        for(i=idx+1;i<events.size();i++)
+        {
+            if(events[i][0]>events[idx][1])
+            {
+                break;
+            }
+        }
+        int op1=recur(dp,idx+1,events,k);
+        int op2=events[idx][2]+recur(dp,i,events,k-1);
+        
+        return dp[idx][k]=max(op1,op2);
+    }
+    int maxValue(vector<vector<int>>& events, int k) {
+        
+        sort(events.begin(),events.end(),[]
+            (vector<int>&a,vector<int>&b)
+             {
+                 if(a[0]==b[0])
+                     return a[1]<b[1];
+                 return  a[0]<b[0];
+             }
+            );
+        int n=events.size();
+        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
+        return recur(dp,0,events,k);
+        
+        
+    }
+};
